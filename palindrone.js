5a495722a4de27a80901157608ae4fa170daf1c3
gregraudenbush <gregraudenbush@gmail.com>
2017-06-12 09:08:27 -0700

    longest Palindrome algo

---
+++ palindrone.js
@@ -0,0 +1,37 @@
+//return longest palindrone of array
+
+
+var arr = ["my", "racecar", "dad"]
+var arr2 = []
+var arr3 = []
+var arr4 = []
+for (var i = 1; i < arr.length; i ++);
+    arr2.push(arr[i]);
+arr = ["my"]
+arr.split("")
+arr4.push(arr)    
+arr2 = ["racecar"]
+arr2.split("") 
+arr4.push(arr) 
+arr3 = ["dad"]
+arr3.split("")
+arr4.push(arr)  
+var arr5 = []
+var arr4 = [[m,y], [r,a,c,e,c,a,r], [d,a,d]]
+arr.split("") //each one
+var pal = true
+
+for (var p = 0; p < arr4.length; i++);
+    for (var i = 0; i < p.length/2; i++ );
+        if arr4[p][i] === p[i][i.length-1];
+        arr5.push(p);
+
+for (var g = 0; g < arr5.length; i++);
+    arr = arr5[g]
+    if arr5[g + 1].length > arr5[g];
+        arr = arr5[g + 1]
+        
+arr.join
+
+
+
